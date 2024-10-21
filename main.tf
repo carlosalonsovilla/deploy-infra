6b9f092ba0349ddc22881fbca3ab3604b1e90857
carlosalonsovilla <cav2589@gmail.com>
2024-10-21 13:27:33 -0400

    Fixed typo.

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "ec2_ssh" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/32"]
+    cidr_blocks = ["0.0.0.0/32"] # Replace with your public IP
   }
 
   egress {
