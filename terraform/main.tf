599fb213282c9cdabf6b9ddf4803b4280c244544
carlosalonsovilla <cav2589@gmail.com>
2024-10-22 21:23:04 -0400

    Added ansible playbook to install and start apache.

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,6 +40,13 @@ resource "aws_security_group" "ec2_ssh" {
     cidr_blocks = ["0.0.0.0/32"] # Replace with your public IP
   }
 
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port = 0
